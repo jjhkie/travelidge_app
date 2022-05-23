fd278a101dacbc5cdee6b84fc5895200c66a4758
DESKTOP-4062TPV\khsk <jjhkie0916@naver.com>
2022-05-23 11:02:39 +0900

    firebase fcm 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flavor/flavor.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:travelidge/binding/init_binding.dart';
 import 'package:travelidge/routes/app_pages.dart';
 import 'package:travelidge/routes/app_routes.dart';
-import 'package:travelidge/ui/nav/nav_bottom.dart';
+import 'firebase_options.dart';
 
-import 'ui/user/screen/user_page.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
 
-void setupApp() {
+void setupApp() async{
   if (Flavor.I.isDevelopment) {
     Flavor.I.getString(Keys.apiUrl);
   }
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(FlavorApp());
 }
 
